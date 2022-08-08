ef2d191b52c62b88a8e7a9997ad84592ba5356dc
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-08-08 14:12:22 +0300

    Update and fix GridManager

--- src/GridManager.ts
+++ src/GridManager.ts
@@ -1,13 +1,17 @@
 import { Tile } from "./Tile";
+
 export class GridManager {
-  tiles: [Tile, Tile];
-  width: 6;
-  height: 6;
-  GenerateGrid() {
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        let spawnedTile = new Tile(x, y);
-        //  tiles[x, y].Add(spawnedTile);
+  tiles: Tile[][];
+  private width: number = 6;
+  private height: number = 6;
+
+  public GenerateGrid(): void {
+    this.tiles = [];
+
+    for (var i: number = 0; i < this.width; i++) {
+      this.tiles[i] = [];
+      for (var j: number = 0; j < this.height; j++) {
+        this.tiles[i][j] = new Tile(i, j);
       }
     }
   }
