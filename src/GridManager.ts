e619e9bb284e4765dd645fd32d8d298e4fa167b8
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-08-09 11:56:50 +0300

    Finished  translating gridManager class
     Add obstacles in the gridManagager.

--- src/GridManager.ts
+++ src/GridManager.ts
@@ -15,4 +15,31 @@ export class GridManager {
       }
     }
   }
+  public AddObstacles(x: number, y: number, obstacle: number): void {
+    this.tiles[x][y].obtacles.push(obstacle);
+  }
+  public GenerateObstacles(): void {
+    this.AddObstacles;
+    this.AddObstacles(1, 5, 4);
+    this.AddObstacles(1, 4, 2);
+    this.AddObstacles(1, 4, 4);
+    this.AddObstacles(1, 3, 2);
+    this.AddObstacles(1, 3, 3);
+
+    this.AddObstacles(2, 3, 1);
+
+    this.AddObstacles(3, 4, 3);
+    this.AddObstacles(3, 3, 3);
+    this.AddObstacles(3, 3, 4);
+    this.AddObstacles(3, 2, 2);
+
+    this.AddObstacles(4, 4, 1);
+    this.AddObstacles(4, 3, 1);
+    this.AddObstacles(4, 2, 3);
+    this.AddObstacles(4, 2, 3);
+
+    this.AddObstacles(5, 4, 4);
+    this.AddObstacles(5, 3, 2);
+    this.AddObstacles(5, 2, 1);
+  }
 }
