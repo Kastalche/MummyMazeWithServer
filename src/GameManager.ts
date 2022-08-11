a36ecbf51b45fd904f42074a78ba78226c262c82
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-08-11 12:42:01 +0300

    Update GameManager
    Translating method

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -1,19 +1,71 @@
+import { BattleController } from "./BattleContoller";
 import { Character } from "./Character";
 import { CharacterMovement } from "./CharacterMovement";
 import { GridManager } from "./GridManager";
+import { IStateController } from "./IStateController";
+import { StartController } from "./StartContoller";
+import { EndController } from "./EndController";
+
+enum GameModes {
+  SinglePlayer,
+  Multiplayer,
+}
+enum GameStates {
+  StartState,
+  BattleState,
+  EndState,
+}
 
 export class GameManager {
   public characters: Array<Character>;
   public characterMovement: CharacterMovement;
   public gridManager: GridManager;
 
-  public AddCharacters(): void {
-    this.characters.push(
-      new Character(this.gridManager.tiles[3][5], true, true)
-    );
+  public mode: GameModes;
+  public state: IStateController;
 
-    this.characters.push(
-      new Character(this.gridManager.tiles[1][2], false, false)
-    );
+  public AddCharacters(): void {
+    switch (this.mode) {
+      case GameModes.SinglePlayer:
+        this.characters.push(
+          new Character(this.gridManager.tiles[3][5], true, true)
+        );
+
+        this.characters.push(
+          new Character(this.gridManager.tiles[1][2], false, false)
+        );
+        break;
+      case GameModes.Multiplayer:
+        this.characters.push(
+          new Character(this.gridManager.tiles[3][5], true, true)
+        );
+        this.characters.push(
+          new Character(this.gridManager.tiles[1][2], false, false)
+        );
+        this.characters.push(
+          new Character(this.gridManager.tiles[1][4], false, false)
+        );
+    }
+  }
+
+  public Transition(newState: GameStates): void {
+    this.state.Destroy();
+    switch (newState) {
+      case GameStates.StartState:
+        this.state = new StartController();
+        break;
+
+      case GameStates.BattleState:
+        this.state = new BattleController();
+        break;
+
+      case GameStates.EndState:
+        this.state = new EndController();
+        break;
+
+      default:
+        break;
+    }
+    this.state.Start();
   }
 }
