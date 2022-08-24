cd72d3082738a4b0183bf27e457c09b0eeef7609
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-08-24 14:14:08 +0300

    Format tabs

--- src/Entities/Character.ts
+++ src/Entities/Character.ts
@@ -1,18 +1,18 @@
 import { Tile } from "./Tile";
 export class Character {
-  id: number;
-  public startPosition: Tile;
-  public currentPosition: Tile;
-  public isMummy: boolean;
-  public isBot: boolean;
+    id: number;
+    public startPosition: Tile;
+    public currentPosition: Tile;
+    public isMummy: boolean;
+    public isBot: boolean;
 
-  constructor(startPosition: Tile, isMummy: boolean, isBot: boolean) {
-    this.startPosition = startPosition;
-    this.isBot = isBot;
-    this.isMummy = isMummy;
-  }
+    constructor(startPosition: Tile, isMummy: boolean, isBot: boolean) {
+        this.startPosition = startPosition;
+        this.isBot = isBot;
+        this.isMummy = isMummy;
+    }
 
-  GoToStartPosition(): void {
-    this.currentPosition = this.startPosition;
-  }
+    GoToStartPosition(): void {
+        this.currentPosition = this.startPosition;
+    }
 }
