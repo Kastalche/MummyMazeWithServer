30e50394b8ab0b82fb74b7e163bd7a525ed33cd3
Siyana <72863822+Kastalche@users.noreply.github.com>
2022-08-25 14:06:22 +0300

    Added a methods for Next Character + AvailableTileForMove, added index for current players

--- src/Game.ts
+++ src/Game.ts
@@ -1,15 +1,18 @@
 import { GameModes, GameServer } from "./GameServer";
 import { Character } from "./Entities/Character";
 import { Player } from "./Player";
-import { CharacterMovement } from "./Entities/CharacterMovement";
 import { GridManager } from "./Entities/GridManager";
+import { Tile } from "./Entities/Tile";
+import { stripVTControlCharacters } from "util";
+
 export class Game {
     private server: GameServer;
     private players: Array<Player>;
     public characters: Array<Character>;
 
     public curentMode: GameModes;
-    public currentCharacter: Character;
+    public currentCharacter: Character = null;
+    public currentCharacterIndex: number;
 
     public gridManager: GridManager;
 
@@ -22,7 +25,7 @@ export class Game {
     }
 
     public AddCharacters(): void {
-        switch (this.mode) {
+        switch (this.curentMode) {
             case GameModes.SinglePlayer:
                 this.characters.push(
                     new Character(this.gridManager.tiles[3][5], true, true)
@@ -45,7 +48,77 @@ export class Game {
         }
     }
 
-    public NextCurrentCharacter() {}
+    public IsAvailableFrom(targetTile: Tile, yourTile: Tile): boolean {
+        if (targetTile.obstacles.length != 0) {
+            if (targetTile.x != yourTile.x) {
+                //if your x is diffrent
+                if (targetTile.x - yourTile.x == -1) {
+                    // u go left => right
+                    if (targetTile.obstacles.includes(3)) return false;
+                    else return true;
+                } else if (targetTile.x - yourTile.x == 1) {
+                    // righ => left
+                    if (targetTile.obstacles.includes(1)) return false;
+                    else return true;
+                }
+            }
 
-    //TODO: ADD is available from because its not used only for bots.
+            if (targetTile.y != yourTile.y) {
+                if (targetTile.y - yourTile.y == 1) {
+                    if (targetTile.obstacles.includes(4))
+                        // up => down
+                        return false;
+                    else return true;
+                } else if (targetTile.y - yourTile.y == -1) {
+                    if (targetTile.obstacles.includes(2))
+                        //down => up
+                        return false;
+                    else return true;
+                }
+            }
+        }
+        return true;
+    }
+
+    public NextCurrentCharacter() {
+        if (this.currentCharacter == null) {
+            this.currentCharacter = this.characters[0];
+            this.currentCharacterIndex = 0;
+        } else {
+            if (this.currentCharacterIndex == this.characters.length) {
+                this.currentCharacterIndex = 0;
+                this.currentCharacter =
+                    this.characters[this.currentCharacterIndex];
+            } else {
+                this.currentCharacterIndex++;
+                this.currentCharacter =
+                    this.characters[this.currentCharacterIndex];
+            }
+        }
+    }
+
+    public CreateCharacterForPlayer(isMummy: boolean): Character {
+        if (isMummy) {
+            var characterToAdd = new Character(
+                this.gridManager.tiles[3][5],
+                true,
+                false
+            );
+            this.characters.push(characterToAdd);
+            return characterToAdd;
+        } else {
+            var characterToAdd = new Character(
+                this.gridManager.tiles[1][2],
+                false,
+                false
+            );
+            this.characters.push(characterToAdd);
+            return characterToAdd;
+        }
+    }
+
+    public CheckForBattleEnd(): boolean {
+        if (this.characters.length <= 1) return true;
+        else return false;
+    }
 }
